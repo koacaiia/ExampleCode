32ebb3fe201a570f90227e112fe4b35a8e8504d4
AdroidYsk <koacaiia@gamil.com>
2021-10-07 10:10:27 +0900

    Init Data

--- app/src/main/java/fine/koacaiia/examplecode/MainActivity.java
+++ app/src/main/java/fine/koacaiia/examplecode/MainActivity.java
@@ -14,56 +14,17 @@ import android.widget.Toast;
 
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
-    SurfaceView surfaceView;
-    SurfaceHolder holder;
-    String[] permission_list = {Manifest.permission.CAMERA};
-    Camera camera;
+public class MainActivity extends AppCompatActivity  {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        requestPermissions(permission_list, 0);
 
-        surfaceView = findViewById(R.id.surfaceView);
-        holder = surfaceView.getHolder();
-        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-        holder.addCallback(this);
 
 
     }
 
-    @Override
-    public void surfaceCreated(@NonNull SurfaceHolder holder) {
-        camera = Camera.open();
-        try {
-            camera.setPreviewDisplay(holder);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        camera.startPreview();
 
-    }
-
-    @Override
-    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        for(int result:grantResults){
-            if(result== PackageManager.PERMISSION_DENIED){
-                Toast.makeText(this, "permission denied", Toast.LENGTH_SHORT).show();
-                return;
-            }
-        }
-    }
 }
